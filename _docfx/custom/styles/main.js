a05bf6f2f3dd9e1fc8bec1536fcfe22bd1d571da
ivaylokenov <ivaylo.kenov@gmail.com>
2016-05-12 01:15:43 +0300

    Updated documentation

--- _docfx/custom/styles/main.js
+++ _docfx/custom/styles/main.js
@@ -2,6 +2,7 @@ $(function() {
 	setTimeout(function() {
 		$('.nav.level1').find('a[href]').each(function (i, el) {
 		  $(el).text($(el).text().replace('MyTested.Mvc.Builders.Contracts.', ''));
+		  $(el).text($(el).text().replace('MyTested.Mvc', 'Common'));
 		});
 	}, 0);
 })
