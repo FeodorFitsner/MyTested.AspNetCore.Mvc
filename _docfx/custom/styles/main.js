5b24f424919877b5c5cd53d3dd2825eabafa9902
ivaylokenov <ivaylo.kenov@gmail.com>
2016-05-13 01:19:23 +0300

    Fixed bug in API reference

--- _docfx/custom/styles/main.js
+++ _docfx/custom/styles/main.js
@@ -1,10 +1,17 @@
 $(function() {
-	do {
+	var interval = setInterval(function() {
 		var elements = $('.nav.level1').find('a[href]');
+		
+		if (elements.length != 0) {
+			clearInterval(interval);
+		}
+		else {
+			return;
+		}
+		
 		elements.each(function (i, el) {
 		  $(el).text($(el).text().replace('MyTested.Mvc.Builders.Contracts.', ''));
 		  $(el).text($(el).text().replace('MyTested.Mvc', 'Common'));
 		});
-	}
-	while (elements.length == 0);
+	}, 200);
 })
